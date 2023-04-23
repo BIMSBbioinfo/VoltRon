7993ca889606c8c5d97dd923c0ee5878e6b49936
Artur-man <artur-man@hotmail.com>
2023-04-23 15:03:23 +0200

    implement register_images function using Rcpp and OpenCV

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -10,18 +10,22 @@ Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
 Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
 #endif
 
-// doing_something
-void doing_something();
-RcppExport SEXP _spaceRover_doing_something() {
+// register_images
+void register_images(Rcpp::String ref_image, Rcpp::String query_image, const float GOOD_MATCH_PERCENT, const int MAX_FEATURES);
+RcppExport SEXP _spaceRover_register_images(SEXP ref_imageSEXP, SEXP query_imageSEXP, SEXP GOOD_MATCH_PERCENTSEXP, SEXP MAX_FEATURESSEXP) {
 BEGIN_RCPP
     Rcpp::RNGScope rcpp_rngScope_gen;
-    doing_something();
+    Rcpp::traits::input_parameter< Rcpp::String >::type ref_image(ref_imageSEXP);
+    Rcpp::traits::input_parameter< Rcpp::String >::type query_image(query_imageSEXP);
+    Rcpp::traits::input_parameter< const float >::type GOOD_MATCH_PERCENT(GOOD_MATCH_PERCENTSEXP);
+    Rcpp::traits::input_parameter< const int >::type MAX_FEATURES(MAX_FEATURESSEXP);
+    register_images(ref_image, query_image, GOOD_MATCH_PERCENT, MAX_FEATURES);
     return R_NilValue;
 END_RCPP
 }
 
 static const R_CallMethodDef CallEntries[] = {
-    {"_spaceRover_doing_something", (DL_FUNC) &_spaceRover_doing_something, 0},
+    {"_spaceRover_register_images", (DL_FUNC) &_spaceRover_register_images, 4},
     {NULL, NULL, 0}
 };
 
