9da5d7192e0ce555d1e02041a5bfd46f7b3bcead
Artur-man <artur-man@hotmail.com>
2023-12-08 11:43:29 +0100

    fix issue with Brute-force matching

--- src/automated_registration.cpp
+++ src/automated_registration.cpp
@@ -160,6 +160,96 @@ cv::Mat reversepreprocessImage(Mat &im, const char* flipflop, const char* rotate
   return imRotate;
 }
 
+void alignImagesBRUTE_old(Mat &im1, Mat &im2, Mat &im1Reg, Mat &im1Overlay, Mat &imMatches, Mat &h, const float GOOD_MATCH_PERCENT, const int MAX_FEATURES,
+                      const bool invert_query, const bool invert_ref,
+                      const char* flipflop_query, const char* flipflop_ref,
+                      const char* rotate_query, const char* rotate_ref)
+{
+  // seed
+  cv::setRNGSeed(0);
+
+  // Convert images to grayscale
+  Mat im1Gray, im2Gray;
+  cvtColor(im1, im1Gray, cv::COLOR_BGR2GRAY);
+  cvtColor(im2, im2Gray, cv::COLOR_BGR2GRAY);
+
+  // Process images
+  Mat im1Proc, im2Proc, im1NormalProc;
+  im1Proc = preprocessImage(im1Gray, invert_query, flipflop_query, rotate_query);
+  im1NormalProc = preprocessImage(im1, FALSE, flipflop_query, rotate_query);
+  im2Proc = preprocessImage(im2Gray, invert_ref, flipflop_ref, rotate_ref);
+
+  // Variables to store keypoints and descriptors
+  std::vector<KeyPoint> keypoints1, keypoints2;
+  Mat descriptors1, descriptors2;
+
+  // Detect ORB features and compute descriptors.
+  Ptr<Feature2D> orb = ORB::create(MAX_FEATURES);
+  orb->detectAndCompute(im1Proc, Mat(), keypoints1, descriptors1);
+  orb->detectAndCompute(im2Proc, Mat(), keypoints2, descriptors2);
+  cout << "DONE: orb based key-points detection and descriptors computation" << endl;
+
+  // Match features.
+  std::vector<DMatch> matches;
+  Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create("BruteForce-Hamming");
+  matcher->match(descriptors1, descriptors2, matches, Mat());
+  cout << "DONE: BruteForce-Hamming - descriptor matching" << endl;
+
+  // Sort matches by score
+  std::sort(matches.begin(), matches.end());
+
+  // Remove not so good matches
+  const int numGoodMatches = matches.size() * GOOD_MATCH_PERCENT;
+  matches.erase(matches.begin()+numGoodMatches, matches.end());
+  cout << "DONE: get good matches by distance thresholding" << endl;
+
+  // Extract location of good matches
+  std::vector<Point2f> points1, points2;
+  for( size_t i = 0; i < matches.size(); i++ )
+  {
+    points1.push_back( keypoints1[ matches[i].queryIdx ].pt );
+    points2.push_back( keypoints2[ matches[i].trainIdx ].pt );
+  }
+
+  // Find homography
+  h = findHomography(points1, points2, RANSAC);
+  cout << "DONE: calculated homography matrix" << endl;
+
+  // Extract location of good matches in terms of keypoints
+  std::vector<KeyPoint> keypoints1_best, keypoints2_best;
+  std::vector<cv::DMatch> goodMatches;
+  for( size_t i = 0; i < matches.size(); i++ )
+  {
+    keypoints1_best.push_back(keypoints1[matches[i].queryIdx]);
+    keypoints2_best.push_back(keypoints2[matches[i].trainIdx]);
+    goodMatches.push_back(cv::DMatch(static_cast<int>(i), static_cast<int>(i), 0));
+  }
+
+  // Draw top matches and good ones only
+  // Mat imMatches;
+  drawMatches(im1, keypoints1_best, im2, keypoints2_best, goodMatches, imMatches);
+
+  // Use homography to warp image
+  Mat im1Warp, im1NormalWarp;
+  warpPerspective(im1Proc, im1Warp, h, im2Proc.size());
+  warpPerspective(im1NormalProc, im1NormalWarp, h, im2Proc.size());
+  im1Reg = reversepreprocessImage(im1NormalWarp, flipflop_ref, rotate_ref);
+  cout << "DONE: warped query image" << endl;
+
+  // // overlay image
+  // Mat im1ColorMap;
+  // cv::applyColorMap(im1Warp, im1ColorMap, cv::COLORMAP_HOT);
+
+  // change color map
+  Mat im1Combine;
+  cv::addWeighted(im2Proc, 0.7, im1Warp, 0.3, 0, im1Combine);
+
+  // return as rgb
+  // cvtColor(im1Combine, im1Reg, cv::COLOR_GRAY2BGR);
+  cvtColor(im1Combine, im1Overlay, cv::COLOR_GRAY2BGR);
+  cvtColor(im2Proc, im2, cv::COLOR_GRAY2BGR);
+}
+
 void alignImagesBRUTE(Mat &im1, Mat &im2, Mat &im1Reg, Mat &im1Overlay, Mat &imMatches, Mat &h, const float GOOD_MATCH_PERCENT, const int MAX_FEATURES)
 {
   // Convert images to grayscale
@@ -219,8 +309,19 @@ void alignImagesBRUTE(Mat &im1, Mat &im2, Mat &im1Reg, Mat &im1Overlay, Mat &imM
 
   // Use homography to warp image
   warpPerspective(im1, im1Reg, h, im2.size());
-  im1Reg = im1Overlay;
+  warpPerspective(im1, im1Overlay, h, im2.size());
+  // im1Reg = im1Overlay;
   cout << "DONE: warped query image" << endl;
+
+  // change color map
+  // Mat im1Combine;
+  // cv::addWeighted(im2Gray, 0.7, im1Reg, 0.3, 0, im1Overlay);
+  //
+  // // return as rgb
+  // cvtColor(im1Combine, im1Overlay, cv::COLOR_GRAY2BGR);
+
+  // cv::imwrite("dest.jpg", im2);
+  // cv::imwrite("source.jpg", im1Overlay);
 }
 
 void alignImagesFLANN(Mat &im1, Mat &im2, Mat &im1Reg, Mat &im1Overlay, Mat &imMatches, Mat &h,
@@ -308,10 +409,6 @@ void alignImagesFLANN(Mat &im1, Mat &im2, Mat &im1Reg, Mat &im1Overlay, Mat &imM
   // cvtColor(im1Combine, im1Reg, cv::COLOR_GRAY2BGR);
   cvtColor(im1Combine, im1Overlay, cv::COLOR_GRAY2BGR);
   cvtColor(im2Proc, im2, cv::COLOR_GRAY2BGR);
-
-  //destination and overlay
-  cv::imwrite("dest.jpg", im2);
-  cv::imwrite("source.jpg", im1Overlay);
 }
 
 // [[Rcpp::export]]
@@ -346,6 +443,8 @@ Rcpp::List automated_registeration_rawvector(Rcpp::RawVector ref_image, Rcpp::Ra
   }
   if(strcmp(method.get_cstring(), "BRUTE-FORCE") == 0){
     cout << "BruteForce-Hamming - descriptor matching" << endl;
+    // alignImagesBRUTE(im, imReference, imReg, imOverlay, imMatches, h, GOOD_MATCH_PERCENT, MAX_FEATURES, invert_query, invert_ref,
+    //                  flipflop_query.get_cstring(), flipflop_ref.get_cstring(), rotate_query.get_cstring(), rotate_ref.get_cstring());
     alignImagesBRUTE(im, imReference, imReg, imOverlay, imMatches, h, GOOD_MATCH_PERCENT, MAX_FEATURES);
   }
 
