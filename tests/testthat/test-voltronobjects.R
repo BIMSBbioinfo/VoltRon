1044b271b0aa1eccb9cd384ed2aa2c7f2468f7f4
Artur-man <artur-man@hotmail.com>
2023-12-17 23:04:29 +0100

    add coordinate tests

--- tests/testthat/test-voltronobjects.R
+++ tests/testthat/test-voltronobjects.R
@@ -1,4 +1,8 @@
-test_that("voltronobjects", {
+test_that("coordinates", {
   data("visium_data")
-  vrSpatialPlot(visium_data, group.by = "Sample", alpha = 0.3)
+  coords <- vrCoordinates(visium_data)
+  coords <- vrCoordinates(visium_data, reg = TRUE)
+  segments <- vrSegments(visium_data)
+  segments <- vrSegments(visium_data, reg = TRUE)
+  expect_equal(1,1L)
 })
