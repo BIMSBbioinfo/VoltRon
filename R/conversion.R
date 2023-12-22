32cc00d0d1d9b51f1e9dbc9e2ec1c0df0a96016f
Artur-man <artur-man@hotmail.com>
2023-12-22 01:03:50 +0100

    allow as.Seurat function to define embeddings in SeuratObject

--- R/conversion.R
+++ R/conversion.R
@@ -164,6 +164,13 @@ as.Seurat.VoltRon <- function(object, cell.assay = NULL, molecule.assay = NULL, 
   # Seurat object
   seu <- Seurat::CreateSeuratObject(counts = data, meta.data = metadata, assay = cell.assay)
 
+  # add embeddings
+  for(embd in vrEmbeddingNames(object)){
+    embd_data <- vrEmbeddings(object, type = embd)
+    colnames(embd_data) <- paste0(embd, 1:ncol(embd_data))
+    seu[[embd]] <- CreateDimReducObject(embd_data, key = paste0(embd, "_"), assay = DefaultAssay(seu))
+  }
+
   # get image objects for each assay
   for(assy in vrAssayNames(object)){
     assay_object <- object[[assy]]
