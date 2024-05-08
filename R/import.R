f905c9a44fe8858ef77d26378b1403408727c0cc
Wei-Che Ko <wcko@users.noreply.github.com>
2024-05-08 15:50:05 -0400

    fix import.R genePS typo for transcript coords

--- R/import.R
+++ R/import.R
@@ -1248,7 +1248,7 @@ importGenePS <- function (dir.path, assay_name = "GenePS", sample_name = NULL, u
       colnames(subcellular_data)[colnames(subcellular_data)=="name"] <- "gene"
       
       # coordinates
-      coords <- as.matrix(transcripts[,c("x", "y")])
+      coords <- as.matrix(subcellular_data[,c("x", "y")])
       colnames(coords) <- c("x", "y")
       if(use_image){
         coords <- coords/scaleparam
