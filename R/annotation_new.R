f289dbd418e1f5d6cb964456730fb31d278536a6
Artur-man <artur-man@hotmail.com>
2024-10-07 01:14:21 +0200

    on disk annotateSpatialData implemented

--- R/annotation_new.R
+++ R/annotation_new.R
@@ -185,22 +185,115 @@ annotateSpatialData_new <- function(object, label = "annotation", assay = NULL, 
     selected_corners <- reactiveVal(data.frame(x = numeric(0), y = numeric(0)))
     ranges <- reactiveValues(x = c(0, imginfo$width), y = c(0, imginfo$height))
     
-    # Point click, double click event and zoom ####
-    manageImageBrushOptions(img, ranges, max.pixel.size, input, output, session)
+    # Zoom Event ####
+    manageImageBrush(img, ranges, max.pixel.size, input, output, session)
     
-    # Point buttons ####
-    # reset corners and remove last point 
+    # Corner Events ####
     observeEvent(input$reset_btn, {
       selected_corners(data.frame(x = numeric(0), y = numeric(0)))
     })
     observeEvent(input$rmvlast_btn, {
       selected_corners(selected_corners()[-nrow(selected_corners()),])
     })
+    manageSelectedCorners(selected_corners, img, ranges, max.pixel.size, input, output, session)
+
+    # Region Events ####
+    n <- length(segments)
+    textboxes <- reactiveVal(if (n > 0) 1:n else numeric(0))
+    if (n > 0) {
+      segment_names <- as.list(segment_names)
+      names(segment_names) <- paste0("region", 1:n)
+      textbox_values <- do.call("reactiveValues", segment_names)
+    } else {
+      textbox_values <- reactiveValues()
+    }
     
-    # image output ####
+    # Dynamically generate UI for textboxes and remove buttons
+    output$textbox_ui <- renderUI({
+      lapply(textboxes(), function(i) {
+        column(12,
+               textInputwithButton(textinputId = paste0("region", i), label = paste0("Region ", i),
+                                   buttoninputId = paste0("remove", i), value = isolate(textbox_values[[paste0("region", i)]]), 
+                                   onclick = sprintf('Shiny.setInputValue("remove", %d)', i))
+               
+        )
+      })
+    })
+  
+    ### update Region text ####
+    
+    observe({
+      lapply(textboxes(), function(i) {
+        observeEvent(input[[paste0("region", i)]], {
+          textbox_values[[paste0("region", i)]] <- isolate(input[[paste0("region", i)]])
+        }, ignoreNULL = FALSE)
+      })
+    })
+    
+    ### remove a Region ####
+    
+    # Observe event to remove textbox when the button is clicked
+    observeEvent(input$remove, {
+      
+      # remove one point
+      selected_corners_list(selected_corners_list()[!(textboxes() == as.numeric(isolate(input$remove)))])
+      
+      # Update the reactive value to remove the textbox
+      textboxes(setdiff(textboxes(), as.numeric(isolate(input$remove))))
+      
+      # Remove the value from textbox_values
+      textbox_values[[paste0("region", as.numeric(input$remove))]] <- NULL
+      
+    }, ignoreInit = TRUE)
+    
+    ### add a Region ####
+    
+    # Observe event to add a new textbox
+    observeEvent(input$addregion_btn, {
+      
+      # Polygon selection
+      if(isolate(input$region_type == "Polygon")){
+        if(nrow(selected_corners()) > 3){
+          
+          # add to region list
+          selected_corners_list(c(selected_corners_list(), list(selected_corners())))
+          
+          # remove selected points
+          selected_corners(data.frame(x = numeric(0), y = numeric(0)))
+          
+          # add buttons
+          new_id <- if (length(textboxes()) == 0) 1 else max(textboxes()) + 1
+          textboxes(c(textboxes(), new_id))
+          textbox_values[[paste0("region", new_id)]] <- ""
+        } else {
+          showNotification("You must selected at least 4 points for each polygon!")
+        }
+      } 
+      
+      # Circle selection
+      if(isolate(input$region_type == "Circle")){
+        if(nrow(selected_corners()) == 2){
+          
+          # add to region list
+          circle <- makeCircleData(selected_corners())
+          selected_corners_list(c(selected_corners_list(), list(circle)))
+          
+          # remove selected points
+          selected_corners(data.frame(x = numeric(0), y = numeric(0)))
+          
+          # add buttons
+          new_id <- if (length(textboxes()) == 0) 1 else max(textboxes()) + 1
+          textboxes(c(textboxes(), new_id))
+          textbox_values[[paste0("region", new_id)]] <- ""
+        } else {
+          showNotification("You must selected only 2 points for each circle!")
+        }
+      }
+    })
+    # Image output ####
     output$image_plot <- renderPlot({
       
-      # get image and plot
+      ## get image ####
       zoom_info <- FromBoxToCrop(cbind(ranges$x, ranges$y), imageinfo = imginfo)
       img <- cropImage(img, zoom_info)
       g <- plotImage(img, max.pixel.size = max.pixel.size) + labs(title = "")
@@ -209,8 +302,124 @@ annotateSpatialData_new <- function(object, label = "annotation", assay = NULL, 
         g <- g + g_spatial 
       }
       
+      # visualize currently selected corners ####
+      transformed_corners <- transformSelectedCorners(selected_corners(), img, ranges, max.pixel.size)
+      if(nrow(transformed_corners) > 0){
+        if(isolate(input$region_type == "Polygon")){
+          g <- g +
+            ggplot2::geom_polygon(aes(x = x, y = y), data = transformed_corners, alpha = input$alpha, color = "red")
+        } else {
+          circle <- makeCircleData(transformed_corners)
+          g <- g +
+            ggforce::geom_ellipse(aes(x0 = as.numeric(x), y0 = as.numeric(y), a = as.numeric(rx), b = as.numeric(ry), angle = 0), 
+                                  data = circle, alpha = input$alpha, color = "red", fill = "red")         
+        }
+        g <- g +
+          ggplot2::geom_point(aes(x = x, y = y), data = transformed_corners, color = "red", shape = 16) 
+      }
+      
+      # visualize already selected regions ####
+      transformed_corners_list <- lapply(selected_corners_list(), transformSelectedCorners, img, ranges, max.pixel.size)
+      if(length(transformed_corners_list) > 0){
+        for (i in 1:length(transformed_corners_list)){
+          cur_corners <- transformed_corners_list[[i]]
+          
+          # visualize regions
+          if(ncol(cur_corners) == 2){
+            g <- g +
+              ggplot2::geom_polygon(aes(x = x, y = y, group = "region"), data = cur_corners, alpha = input$alpha, color = "red") 
+          } else {
+            g <- g +
+              ggforce::geom_ellipse(aes(x0 = as.numeric(x), y0 = as.numeric(y), a = as.numeric(rx), b = as.numeric(ry), angle = 0), 
+                                    data = cur_corners, alpha = input$alpha, color = "red", fill = "red")
+          }
+          
+          # visualize labels
+          if(ncol(cur_corners) == 2){
+            cur_corners <- data.frame(x = mean(cur_corners[,1]), y = max(cur_corners[,2]), region = paste("Region ", isolate(textboxes()[i])))
+          } else {
+            cur_corners <- data.frame(x = cur_corners[,1], y = cur_corners[,2] + cur_corners[,3], region = paste("Region ", isolate(textboxes()[i])))
+          }
+          g <- g +
+            ggrepel::geom_label_repel(mapping = aes(x = x, y = y, label = region), data = cur_corners,
+                                      size = 5, direction = "y", nudge_y = 6, box.padding = 0, label.padding = 1, seed = 1, color = "red")
+          
+        }
+      }
+      
       # return graph
       g
+    })
+    
+    # Return values for the shiny app ####
+    observe({
+      if(length(selected_corners_list()) > 0){
+        shinyjs::show(id = "done")
+      } else {
+        shinyjs::hide(id = "done")
+      }
+    })
+    observeEvent(input$done, {
+      
+      # selected list
+      selected_polygon_list <- selected_corners_list()
+      
+      # collect labels
+      selected_label_list <- sapply(1:length(selected_polygon_list), function(i) input[[paste0("region",i)]])
+      
+      if(length(selected_corners_list()) == 0){
+        showNotification("You have not annotated the data yet!")
+      } else if(any(selected_label_list == "")) {
+        showNotification("Some regions have blank annotations (empty labels!)")
+      } else {
+        
+        ### annotate spatial points ####
+        if(inherits(metadata, "data.table")){
+          spatialpoints <- metadata$id
+        } else {
+          spatialpoints <- rownames(metadata)
+        }
+        
+        new_label <- rep("undefined", length(spatialpoints))
+        names(new_label) <- spatialpoints
+        result_list <- list()
+        for(i in 1:length(selected_polygon_list)){
+          cur_poly <- selected_polygon_list[[i]]
+          if(ncol(cur_poly) > 2){
+            in.list <- point.in.circle(coords[,1], coords[,2], cur_poly[,1], cur_poly[,2], cur_poly[,3])
+          } else {
+            in.list <- sp::point.in.polygon(coords[,1], coords[,2], cur_poly[,1], cur_poly[,2])
+          }
+          new_label[rownames(coords)[!!in.list]] <- selected_label_list[i]
+        }
+        
+        # place annotation to metadata
+        metadata[[label]] <- new_label
+        Metadata(object, assay = sample_metadata[assay, "Assay"]) <- metadata
+        
+        # add polygons to a new assay ####
+        segments <- list()
+        for(i in 1:length(selected_label_list)){
+          segments[[selected_label_list[i]]] <- data.frame(id = i, selected_polygon_list[[i]])
+        }
+        coords <- t(sapply(segments, function(seg){
+          apply(seg[,c("x", "y")], 2, mean)
+        }, simplify = TRUE))
+        new_assay <- formAssay(coords = coords, segments = segments,
+                               type = "ROI",
+                               image = vrImages(object, assay = assay),
+                               main_image = vrMainImage(object[[assay]]),
+                               name = assay)
+        object <- addAssay.VoltRon(object,
+                                   assay = new_assay,
+                                   metadata = data.frame(check.rows = FALSE, row.names = rownames(coords)),
+                                   assay_name = annotation_assay,
+                                   sample = sample_metadata[assay, "Sample"],
+                                   layer = sample_metadata[assay, "Layer"])
+        
+        # stop app and return
+        stopApp(object)
+      }
     })
   }
   
@@ -231,7 +440,7 @@ annotateSpatialData_new <- function(object, label = "annotation", assay = NULL, 
 # Annotation Utilities ####
 ####
 
-manageImageBrushOptions <- function(image, ranges, max.pixel.size, input, output, session){
+manageImageBrush <- function(image, ranges, max.pixel.size, input, output, session){
   imginfo <- getImageInfo(image)
   observeEvent(input$plot_dblclick, {
     brush <- isolate(input$plot_brush)
@@ -245,7 +454,6 @@ manageImageBrushOptions <- function(image, ranges, max.pixel.size, input, output
       limits <- data.frame(x = ranges$x, y = ranges$y)
       width <- limits[2,1]-limits[1,1]
       height <- limits[2,2]-limits[1,2]
-      print(paste("width:", width, "height:", height, sep = " "))
       if(max(height,width) > max.pixel.size){
         if(inherits(image, "Image_Array")){
           n.series <- ImageArray::len(image)
@@ -281,4 +489,191 @@ manageImageBrushOptions <- function(image, ranges, max.pixel.size, input, output
       ranges$y <- c(0, imginfo$height)
     }
   })
+}
+
+manageSelectedCorners <- function(selected_corners, image, ranges, max.pixel.size, input, output, session){
+  observeEvent(input$plot_click, {
+    brush <- isolate(input$plot_brush)
+    if(is.null(brush)) {
+      
+      # if a circle, dont allow more than two points
+      if(isolate(input$region_type == "Circle")){
+        if(nrow(selected_corners()) == 2){
+          selected_corners(data.frame(x = numeric(0), y = numeric(0)))
+        }
+      }
+      
+      # get points
+      click <- input$plot_click
+      pts <- c(click$x, click$y)
+
+      # correct for zoom effect
+      limits <- data.frame(x = ranges$x, y = ranges$y)
+      width <- limits[2,1]-limits[1,1]
+      height <- limits[2,2]-limits[1,2]
+      if(max(height,width) > max.pixel.size){
+        if(inherits(image, "Image_Array")){
+          n.series <- ImageArray::len(image)
+          cur_width <- width
+          cur_height <- height
+          for(ii in 2:n.series){
+            cur_width <- width/(2^(ii-1))
+            cur_height <- height/(2^(ii-1))
+            if(max(cur_height, cur_width) <= max.pixel.size){
+              break
+            }
+          }
+          pts <- pts*width/ceiling(cur_width)
+        } else {
+          pts <- pts*width/max.pixel.size
+        }
+      }
+      
+      # correct for offset effect
+      pts[1] <- pts[1] + limits[1,1]
+      pts[2] <- pts[2] + limits[1,2]
+
+      # Append new point to the data frame
+      new_point <- data.frame(x = pts[1], y = pts[2])
+      
+      # add new point
+      selected_corners(rbind(selected_corners(), new_point))
+    }
+  })
+}
+
+transformSelectedCorners <- function(selected_corners, image, ranges, max.pixel.size){
+  
+  # if empty, return
+  if(nrow(selected_corners) == 0){
+    return(selected_corners)
+  }
+  selected_corners <- as.matrix(selected_corners)
+  
+  # circle settings
+  circle <- FALSE
+  if(ncol(selected_corners) > 2)
+    circle <- TRUE
+  
+  # get circle radius 
+  if(circle){
+    selected_radius <- selected_corners[,3:4, drop = FALSE]
+    selected_corners <- selected_corners[,1:2, drop = FALSE]
+  }
+  
+  # get image info
+  imginfo <- getImageInfo(image)
+  
+  # correct for offset effect
+  limits <- cbind(ranges$x, ranges$y)
+  selected_corners <- selected_corners - matrix(rep(limits[1,], nrow(selected_corners)), nrow = nrow(selected_corners), byrow = T)
+
+  # correct for zoom effect
+  width <- limits[2,1]-limits[1,1]
+  height <- limits[2,2]-limits[1,2]
+  if(max(height,width) > max.pixel.size){
+    if(inherits(image, "Image_Array")){
+      n.series <- ImageArray::len(image)
+      cur_width <- width
+      cur_height <- height
+      for(ii in 2:n.series){
+        cur_width <- width/(2^(ii-1))
+        cur_height <- height/(2^(ii-1))
+        if(max(cur_height, cur_width) <= max.pixel.size){
+          break
+        }
+      }
+      selected_corners <- selected_corners*ceiling(cur_width)/width
+      if(circle){
+        selected_radius <- selected_radius*ceiling(cur_width)/width
+      }
+    } else {
+      selected_corners <- selected_corners*max.pixel.size/width
+      if(circle){
+        selected_radius <- selected_radius*max.pixel.size/width
+      }
+    }
+  }
+  
+  # make data frame
+  if(circle){
+    selected_corners <- as.data.frame(cbind(selected_corners,selected_radius))
+    colnames(selected_corners) <- c("x", "y", "rx", "ry")
+  } else {
+    selected_corners <- as.data.frame(selected_corners)
+    colnames(selected_corners) <- c("x", "y") 
+  }
+  
+  # return
+  selected_corners
+}
+
+####
+# Shiny Utilities ####
+####
+
+#' internal Text input with button
+#' 
+#' Reproduced since it is not exported in the Shiny namespace.
+#' 
+#' @importFrom shiny tags validateCssUnit
+#' 
+#' @keywords internal
+textInputwithButton <- function (textinputId, label, buttoninputId, value = "", width = NULL, placeholder = NULL, ...) 
+{
+  textvalue <- restoreInput(id = textinputId, default = value)
+  buttonvalue <- restoreInput(id = buttoninputId, default = NULL)
+  div(class = "form-group shiny-input-container", 
+      style =  css(width = shiny::validateCssUnit(width), display = "inline-block"),
+      shinyInputLabel(textinputId, label), 
+      shiny::tags$input(id = textinputId, 
+                        style = css(width = "80%", float = "left"),
+                        type = "text", class = "shiny-input-text form-control", 
+                        value = textvalue, placeholder = placeholder),
+      shiny::tags$button(id = buttoninputId, 
+                         style = css(width = "20%", float = "left"),
+                         type = "button", class = "btn btn-default action-button", 
+                         `data-val` = buttonvalue, disabled = NULL, list(shiny::icon("trash")), ...)
+  )
+}
+
+#' Shiny's internal \code{shinyInputLabel} function
+#' 
+#' Reproduced since it is not exported in the Shiny namespace.
+#' 
+#' @importFrom shiny tags
+#' 
+#' @keywords internal
+shinyInputLabel <- function(inputId, label=NULL) {
+  shiny::tags$label(label,
+                    class = "control-label",
+                    class = if (is.null(label)) "shiny-label-null",
+                    `for` = inputId
+  )
+}
+
+####
+# Auxiliary ####
+####
+
+#' makeCircleData
+#' 
+#' creates circle ROI from selected corners
+#' 
+#' @keywords internal
+makeCircleData <- function(selected_corners) {
+  radius <- sqrt((selected_corners[1,1] - selected_corners[2,1])^2 + (selected_corners[1,2] - selected_corners[2,2])^2)
+  data.frame(x = selected_corners[1,1], y = selected_corners[1,2], rx = radius, ry = radius)
+}
+
+#' point.in.circle
+#' 
+#' checks if points are in the defined circle ROI with a (x,y) center and radius
+#' 
+#' @keywords internal
+point.in.circle <- function(x,y,circle.x, circle.y, radius) {
+  x <- x - circle.x
+  y <- y - circle.y
+  distall <- sqrt((x - circle.x)^2 + (y - circle.y)^2)
+  return(distall < radius)
 }
