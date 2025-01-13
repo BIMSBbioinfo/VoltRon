cb9c0640eee5192c072876b0e9e3c83108510c60
Artur-man <artur-man@hotmail.com>
2025-01-13 14:35:34 +0100

    cancel conversion testing for now

--- tests/testthat/test_conversion.R
+++ tests/testthat/test_conversion.R
@@ -14,25 +14,25 @@ test_that("as.AnnData", {
   h5ad_file <- tempfile(fileext = ".h5ad")
   zarr_file <- tempfile(fileext = ".zarr")
 
-  # xenium to anndata
-  as.AnnData(xenium_data, file = h5ad_file)
-  as.AnnData(xenium_data, file = h5ad_file, assay = "Assay1")
-  as.AnnData(xenium_data, file = h5ad_file, flip_coordinates = TRUE)
-
-  # visium to anndata
-  as.AnnData(visium_data, file = h5ad_file)
-  as.AnnData(visium_data, file = h5ad_file, assay = "Assay1")
-  as.AnnData(visium_data, file = h5ad_file, flip_coordinates = TRUE)
-
-  # xenium to anndata
-  as.AnnData(xenium_data, file = zarr_file)
-  as.AnnData(xenium_data, file = zarr_file, assay = "Assay1")
-  as.AnnData(xenium_data, file = zarr_file, flip_coordinates = TRUE)
-
-  # visium to anndata
-  as.AnnData(visium_data, file = zarr_file)
-  as.AnnData(visium_data, file = zarr_file, assay = "Assay1")
-  as.AnnData(visium_data, file = zarr_file, flip_coordinates = TRUE)
+  # # xenium to anndata
+  # as.AnnData(xenium_data, file = h5ad_file)
+  # as.AnnData(xenium_data, file = h5ad_file, assay = "Assay1")
+  # as.AnnData(xenium_data, file = h5ad_file, flip_coordinates = TRUE)
+  # 
+  # # visium to anndata
+  # as.AnnData(visium_data, file = h5ad_file)
+  # as.AnnData(visium_data, file = h5ad_file, assay = "Assay1")
+  # as.AnnData(visium_data, file = h5ad_file, flip_coordinates = TRUE)
+  # 
+  # # xenium to anndata
+  # as.AnnData(xenium_data, file = zarr_file)
+  # as.AnnData(xenium_data, file = zarr_file, assay = "Assay1")
+  # as.AnnData(xenium_data, file = zarr_file, flip_coordinates = TRUE)
+  # 
+  # # visium to anndata
+  # as.AnnData(visium_data, file = zarr_file)
+  # as.AnnData(visium_data, file = zarr_file, assay = "Assay1")
+  # as.AnnData(visium_data, file = zarr_file, flip_coordinates = TRUE)
 
   # clean file
   file.remove(h5ad_file)
