c1fa6a95ca1cba6373b81ab5bcfbb05f846bba7b
Artur-man <artur-man@hotmail.com>
2025-10-18 20:47:22 +0200

    fix simpleitk test

--- tests/testthat/test-registeration.R
+++ tests/testthat/test-registeration.R
@@ -1,7 +1,9 @@
 mapping_parameters_file <- system.file(package = "VoltRon", "extdata/xenium_mappingparameters.rds")
 mapping_parameters <- readRDS(mapping_parameters_file)
+mapping_parameters$nonrigid <- "OpenST"
 mapping_parameters_nonrigid_file <- system.file(package = "VoltRon", "extdata/xenium_mappingparameters_nonrigid.rds")
 mapping_parameters_nonrigid <- readRDS(mapping_parameters_nonrigid_file)
+mapping_parameters_nonrigid$nonrigid <- "OpenST"
 
 test_that("registeration rigid + affine", {
   
@@ -73,6 +75,41 @@ test_that("registeration non-rigid", {
                       mapping_parameters = mapping_parameters_nonrigid, 
                       interactive = FALSE)
   
+  # return
+  expect_equal(1,1L)
+})
+
+test_that("registeration non-rigid simpleitk", {
+  
+  # get data
+  data("xenium_data")
+  
+  # simpleitk 
+  mapping_parameters_nonrigid$nonrigid <- "SimpleITK"
+  
+  # non-interactive
+  registerSpatialData(reference_spatdata = xenium_data, 
+                      query_spatdata = xenium_data, 
+                      mapping_parameters = mapping_parameters_nonrigid, 
+                      interactive = FALSE)
+  
+  # change parameters
+  mapping_parameters_nonrigid$Matcher <- "BRUTE-FORCE"
+  registerSpatialData(reference_spatdata = xenium_data, 
+                      query_spatdata = xenium_data, 
+                      mapping_parameters = mapping_parameters_nonrigid, 
+                      interactive = FALSE)
+  mapping_parameters_nonrigid$Method <- "Homography"
+  registerSpatialData(reference_spatdata = xenium_data, 
+                      query_spatdata = xenium_data, 
+                      mapping_parameters = mapping_parameters_nonrigid, 
+                      interactive = FALSE)
+  mapping_parameters_nonrigid$Method <- "Homography"
+  mapping_parameters_nonrigid$Matcher <- "FLANN"
+  registerSpatialData(reference_spatdata = xenium_data, 
+                      query_spatdata = xenium_data, 
+                      mapping_parameters = mapping_parameters_nonrigid, 
+                      interactive = FALSE)
   
   # return
   expect_equal(1,1L)
